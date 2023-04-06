0d9a8551dafe99564a34de06159ad95c90780eff
geneyee <geneyeey@gmail.com>
2023-04-06 17:18:35 +0900

    complete:TodolistListResponseDto.java

--- src/main/java/com/toy/todolist/web/dto/TodolistListResponseDto.java
+++ src/main/java/com/toy/todolist/web/dto/TodolistListResponseDto.java
@@ -2,21 +2,19 @@ package com.toy.todolist.web.dto;
 
 import com.toy.todolist.domain.todo.Todolist;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @Getter
-public class TodolistResponseDto {
+public class TodolistListResponseDto {
 
     private Long id;
     private String content;
     private Boolean isCompleted;
 
-    //dto class 생성자 entity 파라미터로 사용(db 조회시)
-    public TodolistResponseDto(Todolist entity) {
+    public TodolistListResponseDto(Todolist entity) {
         this.id = entity.getId();
         this.content = entity.getContent();
         this.isCompleted = entity.getIsCompleted();
+
     }
 
 
